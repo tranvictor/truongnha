ddf8b81887b0a0f4957955b9be1a472cce3d61c1
Vu Tran <vu.tran54@gmail.com>
2012-09-08 08:52:17 +0700

    remove sending sms permission on demo school

--- sms/utils.py
+++ sms/utils.py
@@ -3,6 +3,7 @@ from models import sms
 import settings
 import os
 from django.core import mail
+from suds.client import Client
 
 def to_ascii(string):
     result = ''
@@ -46,52 +47,48 @@ def send_email(subject, message, from_addr=None, to_addr=[]):
             settings.EMAIL_HOST_USER,
             to_addr)
 
-def sendSMS(phone,content,user, save_to_db = True):
+def sendSMS(phone, content, user, save_to_db=True):
     phone = checkValidPhoneNumber(phone)
-    try:
-        school = user.userprofile.organization
-        if phone:
-            from suds.client import Client
-            url = settings.SMS_WSDL_URL
-            username = settings.WSDL_USERNAME
-            password = settings.WSDL_PASSWORD
-            mt_username = settings.MT_USERNAME
-            mt_password = settings.MT_PASSWORD
-            content = to_ascii(u'Truong ' + unicode(school) + u' thong bao:' + '\n' + content)
-            s = None
-            if save_to_db:
-                s = sms(phone=phone, content=content,
-                        sender=user, recent=True, success=True)
-                s.save()
-            client = Client(url, username = username, password = password)
-            message = \
-        '''<?xml version="1.0" encoding="UTF-8"?>
+    school = user.userprofile.organization
+    if school.id in [42, 44]: raise Exception('NotAllowedSMS')
+    if phone:
+        url = settings.SMS_WSDL_URL
+        username = settings.WSDL_USERNAME
+        password = settings.WSDL_PASSWORD
+        mt_username = settings.MT_USERNAME
+        mt_password = settings.MT_PASSWORD
+        content = to_ascii(u'Truong ' + unicode(school) + u' thong bao:' + '\n' + content)
+        s = None
+        if save_to_db:
+            s = sms(phone=phone, content=content,
+                    sender=user, recent=True, success=True)
+            s.save()
+        client = Client(url, username = username, password = password)
+        message = \
+    '''<?xml version="1.0" encoding="UTF-8"?>
 <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-  <soap12:Body>
-    <InsertMT xmlns="http://tempuri.org/">
-      <User>%s</User>
-      <Pass>%s</Pass>
-      <CPCode>160</CPCode>
-      <RequestID>4</RequestID>
-      <UserID>%s</UserID>
-      <ReceiveID>%s</ReceiveID>
-      <ServiceID>8062</ServiceID>
-      <CommandCode>CNHN1</CommandCode>
-      <ContentType>0</ContentType>
-	<Info>%s</Info>
-    </InsertMT>
-  </soap12:Body>
+<soap12:Body>
+<InsertMT xmlns="http://tempuri.org/">
+  <User>%s</User>
+  <Pass>%s</Pass>
+  <CPCode>160</CPCode>
+  <RequestID>4</RequestID>
+  <UserID>%s</UserID>
+  <ReceiveID>%s</ReceiveID>
+  <ServiceID>8062</ServiceID>
+  <CommandCode>CNHN1</CommandCode>
+  <ContentType>0</ContentType>
+<Info>%s</Info>
+</InsertMT>
+</soap12:Body>
 </soap12:Envelope>''' % (mt_username, mt_password, phone, phone, content)
-            result = client.service.InsertMT(__inject= {'msg': str(message)})
-            if result != '1' and save_to_db:
-                s.success = False
-                s.save()
-            return result
-        else:
-            raise Exception("InvalidPhoneNumber")
-    except Exception as e:
-        print e
-        raise e
+        result = client.service.InsertMT(__inject= {'msg': str(message)})
+        if result != '1' and save_to_db:
+            s.success = False
+            s.save()
+        return result
+    else:
+        raise Exception("InvalidPhoneNumber")
 
 def checkValidPhoneNumber(phone):
     if not int(phone[0]):
