a9d83e5bbb30cd93fd8e58a0741a5bf48fdbfd15
luult_54 <luulethe@gmail.com>
2012-09-18 23:41:58 +0700

    update unittest for add mark

--- school/unittests/mark_unittests.py
+++ school/unittests/mark_unittests.py
@@ -2,7 +2,7 @@
 #encoding:UTF-8
 from django.core.urlresolvers import reverse
 from base_tests import SchoolSetupTest,AddStudentTest
-from school.models import *
+from school.models import Mark
 import simplejson
 from datetime import date
 
@@ -14,23 +14,22 @@ class MarkTest(AddStudentTest):
             if c.number_of_pupils() > 0:
                 break
         s = c.subject_set.all()[0]
-        m = Mark.objects.get(subject_id=s, term_id__number=1)
-        print m
-        #print "ok1"
-        data = '4//0/false/' + str(m.id) + ':1*2*:2*1.9*'
-        response = self.client.post(
-            reverse('save_mark'),
-                {
-                'request_type':u'sms',
-                'data':data,
-            },
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
-        )
-
-        #m1 = Mark.objects.get(subject_id=s, term_id__number=1)
-        #print m1.diem
-        #print m1.toArrayMark()
+        mark_list = Mark.objects.filter(subject_id=s)
+        for m in mark_list:
+            data = '4//0/false/' + str(m.id) + ':1*2*9*10*11*17*18*19*25*26:1*2*3*4*5*6*7*8*9'
+            response = self.client.post(
+                reverse('save_mark'),
+                    {
+                    'request_type':u'sms',
+                    'data':data,
+                },
+                HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+            )
         self.assertEqual(response.status_code, 200)
         print 'Going to check response content type'
         self.assertEqual(response['Content-Type'], 'json')
+        mark_list = Mark.objects.filter(subject_id=s)
+        for m in mark_list:
+            self.assertEqual(m.diem,'1*2|3*4*5|6*7*8')
+            self.assertEqual(m.ck,9)
 
