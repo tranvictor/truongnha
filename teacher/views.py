09d6c9136af78b11250f8ce8c8a86c9cf5f1a643
Vu Tran <vu.tran54@gmail.com>
2012-11-05 12:55:16 +0700

    update base view class for teacher app

--- teacher/views.py
+++ teacher/views.py
@@ -10,6 +10,49 @@ from django import forms
 from teacher import models
 from sms.utils import send_email
 
+NOT_ALLOWED_TEACHER_TEMPLATE = os.path.join('teacher', 'not-allowed.html')
+
+class BaseTeacherView(TemplateView):
+    def __init__(self, *args, **kwargs):
+        super(BaseTeacherView, self).__init__(*args, **kwargs)
+        # Store teacher_id for further usage and checking
+        # allow_other_teacher is False means that teacher's page
+        # can't be accessed by other users
+        self.teacher_id = None
+        self.allow_other_teacher = False
+        print 'invoke constructor'
+        
+    def _get_teacher(self):
+        return self.request.user.teachers
+
+    def _right_teacher_id(self, teacher_id):
+        return teacher_id == self.teacher.id
+
+    def dispatch(self, request, *args, **kwargs):
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        # Before calling the actual view, try to validate teacher.
+        # If the page belongs to other teacher rather than user, return an
+        # error by default. This default behavior can be disabled if
+        # self.allow_other_teacher set to True
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(),
+                    self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        self.request = request
+        self.teacher_id = kwargs['teacher_id']
+        self.teacher = self._get_teacher()
+        if (not self.allow_other_teacher
+                and not self._right_teacher_id(self.teacher_id)):
+            return render_to_response(NOT_ALLOWED_TEACHER_TEMPLATE,
+                    {}, context_instance=RequestContext(request))
+        self.args = args
+        self.kwargs = kwargs
+        print self.teacher_id, 'before view'
+        return handler(request, *args, **kwargs)
+
 class RegisterView(TemplateView):
     template_name = os.path.join('teacher', 'register.html')
 
@@ -36,6 +79,7 @@ class RegisterView(TemplateView):
                 {'register_form': register_form},
                 context_instance=RequestContext(request))
 
-class IndexView(TemplateView):
-    def get(self, request):
-        return HttpResponse('')
+class IndexView(BaseTeacherView):
+    def get(self, *args, **kwargs):
+        print 'in view'
+        return HttpResponse('', mimetype='json')
