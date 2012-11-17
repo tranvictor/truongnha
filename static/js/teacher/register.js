97e43395b6884208f5115e71911aaa4eb97fdca0
loilt_54 <loi.luuthe@gmail.com>
2012-11-18 00:24:52 +0700

    reset captcha

--- static/js/teacher/register.js
+++ static/js/teacher/register.js
@@ -76,10 +76,12 @@ $(document).ready(function () {
                         window.location.replace(json.redirect);
                     } else {
                         $("#notify").showNotification(json.message, 2000);
+                        Recaptcha.reload();
                     }
                 },
                 error:function () {
                     $("#notify").showNotification("Gặp lỗi khi gửi đăng ký", 2000);
+                    Recaptcha.reload();
                 }
             };
             $.ajax(arg);
