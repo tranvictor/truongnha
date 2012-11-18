d40c1865b535548db644dbf4cd67d3139e8e0373
Vu Tran <vu.tran54@gmail.com>
2012-11-19 05:13:30 +0700

    update view, reconstruct the views

---
+++ teacher/base.py
@@ -0,0 +1,171 @@
+# coding: utf-8
+import os
+import simplejson
+from django.views.generic import TemplateView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.template.loader import render_to_string
+
+NOT_ALLOWED_TEACHER_TEMPLATE = os.path.join('teacher', 'not-allowed.html')
+
+class BaseTeacherView(TemplateView):
+    def __init__(self, *args, **kwargs):
+        super(BaseTeacherView, self).__init__(*args, **kwargs)
+        # Store teacher_id for further usage and checking
+        # allow_other_teacher is False means that teacher's page
+        # can't be accessed by other users
+        self.teacher_id = None
+        self.allow_other_teacher = False
+        
+    def _get_teacher(self):
+        return self.request.user.teachers
+
+    def _right_teacher_id(self, teacher_id):
+        return teacher_id == unicode(self.teacher.id)
+
+    def reverse(self, *args, **kwargs):
+        # Do the same work with Django reverse method but add
+        # valid teacher_id to **kwargs as an extra work
+        kwargs['kwargs']['teacher_id'] = self.teacher_id
+        return reverse(*args, **kwargs)
+    
+    def request_type_not_allowed(self, request, *args, **kwargs):
+        return render_to_response(NOT_ALLOWED_TEACHER_TEMPLATE,
+                {}, context_instance=RequestContext(request))
+
+    def dispatch(self, request, *args, **kwargs):
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        # Before calling the actual view, try to validate teacher.
+        # If the page belongs to other teacher rather than user, return an
+        # error by default. This default behavior can be disabled if
+        # self.allow_other_teacher set to True
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(),
+                    self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        self.request = request
+        self.teacher_id = kwargs['teacher_id']
+        self.teacher = self._get_teacher()
+        if not self.teacher_id:
+            self.teacher_id = self.teacher.id
+            return HttpResponseRedirect(self.reserve('teacher_index'))
+        else:
+            if (not self.allow_other_teacher
+                    and not self._right_teacher_id(self.teacher_id)):
+                return render_to_response(NOT_ALLOWED_TEACHER_TEMPLATE,
+                        {}, context_instance=RequestContext(request))
+        self.args = args
+        self.kwargs = kwargs
+        return handler(request, *args, **kwargs)
+
+    def _menu(self, *args, **kwargs):
+        # This method return objects attached to menu
+        classes = self.teacher.class_set.order_by('-created')
+        return {'classes': classes,
+                'user_teacher': self.teacher}
+
+    def _get(self, *args, **kwargs):
+        # This method return all the needs to render reponse.
+        # Every view class inheritted from this class
+        # SHOULD OVERRIDE this method to deal with request processing.
+        # SHOULD NOT return NONE, return {} instead
+        raise Exception('NotImplemented')
+
+    def _post(self, *args, **kwargs):
+        # This method return all the needs to notify after post request.
+        # Every view class inheritted from this class
+        # SHOULD OVERRIDE this method to deal with request processing.
+        # SHOULD NOT return NONE, return {} instead
+        raise Exception('NotImplemented')
+
+    def get(self, *args, **kwargs):
+        # This method handles get request:
+        # If the request is via ajax, it's assumed to be called 
+        # inside client's javascript. In this case, we return 
+        # only the content part of the view (not include menu part)
+        # in json: {'success': True, 'content': html_string}
+        # Otherwise, the normal HttpResponse will be returned
+        # (including menu and content part)
+        # This situation appears when user access directly via url
+        # (not from ajax)
+        #
+        #
+        # THIS METHOD SHOULD NOT BE OVERRIDED
+        menus = self._menu(*args, **kwargs)
+        params = self._get(*args, **kwargs)
+        # If params is instance of HttpReponse, return it
+        # because it must be the error page
+        if isinstance(params, HttpResponse): return params
+        params.update(menus)
+        if self.request.is_ajax():
+            params['full'] = False
+            res = render_to_string(self.template_name, params,
+                    context_instance=RequestContext(self.request))
+            return HttpResponse(simplejson.dumps(
+                {'success': True, 'content': res}),
+                mimetype='json')
+
+        else:
+            params['full'] = True
+            print self.template_name
+            return render_to_response(self.template_name,
+                    params, context_instance=RequestContext(self.request))
+
+    def post(self, *args, **kwargs):
+        # This method handles post request:
+        # If the request is via ajax, it's assumed to be called 
+        # inside client's javascript. In this case, we return 
+        # the result, message, error list ...
+        # in json: {'success': True/False,
+        # 'message': message_string,
+        # 'error_list': error_list,
+        # ........................}
+        # Otherwise, the normal HttpResponse will be returned
+        # (including menu and content part)
+        # This situation appears when user disable their javascript
+        # functionalities (not from ajax)
+        #
+        # THIS METHOD SHOULD NOT BE OVERRIDED
+        params = self._post(*args, **kwargs)
+        print 'params', params
+        if self.request.is_ajax():
+            return HttpResponse(simplejson.dumps(
+                params), mimetype='json')
+
+        else:
+            # This this a dump return, it just stands here to
+            # handle nonajax post which is usually not supported
+            return HttpResponse(unicode(params))
+
+# This class provide 2 method to extract request type
+# and call appropriate method to handle particular request type
+class RestfulView:
+    def _get(self, *args, **kwargs):
+        try:
+            req_type = kwargs['request_type']
+        except KeyError:
+            req_type = 'view'
+        if req_type in self.request_type:
+            handler = getattr(self, '_get_' + req_type,
+                    self.request_type_not_allowed)
+        else:
+            handler = self.request_type_not_allowed
+        return handler(*args, **kwargs)
+    
+    def _post(self, *args, **kwargs):
+        try:
+            req_type = kwargs['request_type']
+        except KeyError:
+            req_type = 'view'
+        if req_type in self.request_type:
+            handler = getattr(self, '_post_' + req_type,
+                    self.request_type_not_allowed)
+        else:
+            handler = self.request_type_not_allowed
+        return handler(*args, **kwargs)
+
--- teacher/views.py
+++ teacher/views.py
@@ -10,7 +10,6 @@ from django.views.generic import TemplateView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
-from django.template.loader import render_to_string
 from django.shortcuts import render_to_response
 from django import forms
 from django.contrib.auth.models import User
@@ -19,133 +18,8 @@ from django.core.exceptions import ObjectDoesNotExist
 import validations
 from teacher.models import Person, Teacher, Register, Class
 from sms.utils import send_email
+from teacher.base import BaseTeacherView, RestfulView
 import settings
-
-NOT_ALLOWED_TEACHER_TEMPLATE = os.path.join('teacher', 'not-allowed.html')
-
-class BaseTeacherView(TemplateView):
-    def __init__(self, *args, **kwargs):
-        super(BaseTeacherView, self).__init__(*args, **kwargs)
-        # Store teacher_id for further usage and checking
-        # allow_other_teacher is False means that teacher's page
-        # can't be accessed by other users
-        self.teacher_id = None
-        self.allow_other_teacher = False
-        
-    def _get_teacher(self):
-        return self.request.user.teachers
-
-    def _right_teacher_id(self, teacher_id):
-        return teacher_id == unicode(self.teacher.id)
-
-    def reverse(self, *args, **kwargs):
-        # Do the same work with Django reverse method but add
-        # valid teacher_id to **kwargs as an extra work
-        kwargs['kwargs']['teacher_id'] = self.teacher_id
-        return reverse(*args, **kwargs)
-
-    def dispatch(self, request, *args, **kwargs):
-        # Try to dispatch to the right method; if a method doesn't exist,
-        # defer to the error handler. Also defer to the error handler if the
-        # request method isn't on the approved list.
-        # Before calling the actual view, try to validate teacher.
-        # If the page belongs to other teacher rather than user, return an
-        # error by default. This default behavior can be disabled if
-        # self.allow_other_teacher set to True
-        if request.method.lower() in self.http_method_names:
-            handler = getattr(self, request.method.lower(),
-                    self.http_method_not_allowed)
-        else:
-            handler = self.http_method_not_allowed
-        self.request = request
-        self.teacher_id = kwargs['teacher_id']
-        self.teacher = self._get_teacher()
-        if not self.teacher_id:
-            self.teacher_id = self.teacher.id
-            return HttpResponseRedirect(self.reserve('teacher_index'))
-        else:
-            if (not self.allow_other_teacher
-                    and not self._right_teacher_id(self.teacher_id)):
-                return render_to_response(NOT_ALLOWED_TEACHER_TEMPLATE,
-                        {}, context_instance=RequestContext(request))
-        self.args = args
-        self.kwargs = kwargs
-        return handler(request, *args, **kwargs)
-
-    def _menu(self, *args, **kwargs):
-        # This method return objects attached to menu
-        classes = self.teacher.class_set.order_by('-created')
-        return {'classes': classes,
-                'user_teacher': self.teacher}
-
-    def _get(self, *args, **kwargs):
-        # This method return all the needs to render reponse.
-        # Every view class inheritted from this class
-        # SHOULD OVERRIDE this method to deal with request processing.
-        # SHOULD NOT return NONE, return {} instead
-        raise Exception('NotImplemented')
-
-    def _post(self, *args, **kwargs):
-        # This method return all the needs to notify after post request.
-        # Every view class inheritted from this class
-        # SHOULD OVERRIDE this method to deal with request processing.
-        # SHOULD NOT return NONE, return {} instead
-        raise Exception('NotImplemented')
-
-    def get(self, *args, **kwargs):
-        # This method handles get request:
-        # If the request is via ajax, it's assumed to be called 
-        # inside client's javascript. In this case, we return 
-        # only the content part of the view (not include menu part)
-        # in json: {'success': True, 'content': html_string}
-        # Otherwise, the normal HttpResponse will be returned
-        # (including menu and content part)
-        # This situation appears when user access directly via url
-        # (not from ajax)
-        #
-        #
-        # THIS METHOD SHOULD NOT BE OVERRIDED
-        menus = self._menu(*args, **kwargs)
-        params = self._get(*args, **kwargs)
-        params.update(menus)
-        if self.request.is_ajax():
-            params['full'] = False
-            res = render_to_string(self.template_name, params,
-                    context_instance=RequestContext(self.request))
-            return HttpResponse(simplejson.dumps(
-                {'success': True, 'content': res}),
-                mimetype='json')
-
-        else:
-            params['full'] = True
-            return render_to_response(self.template_name,
-                    params, context_instance=RequestContext(self.request))
-
-    def post(self, *args, **kwargs):
-        # This method handles post request:
-        # If the request is via ajax, it's assumed to be called 
-        # inside client's javascript. In this case, we return 
-        # the result, message, error list ...
-        # in json: {'success': True/False,
-        # 'message': message_string,
-        # 'error_list': error_list,
-        # ........................}
-        # Otherwise, the normal HttpResponse will be returned
-        # (including menu and content part)
-        # This situation appears when user disable their javascript
-        # functionalities (not from ajax)
-        #
-        # THIS METHOD SHOULD NOT BE OVERRIDED
-        params = self._post(*args, **kwargs)
-        print 'params', params
-        if self.request.is_ajax():
-            return HttpResponse(simplejson.dumps(
-                params), mimetype='json')
-
-        else:
-            # This this a dump return, it just stands here to
-            # handle nonajax post which is usually not supported
-            return HttpResponse(unicode(params))
 
 
 class IndexView(BaseTeacherView):
@@ -154,8 +28,7 @@ class IndexView(BaseTeacherView):
     def _get(*args, **kwargs):
         return {}
     
-class ClassView(IndexView):
-    template_name = os.path.join('teacher', 'class.html')
+class ClassView(RestfulView, BaseTeacherView):
     request_type = ['view', 'create', 'remove', 'modify']
 
     class ClassForm(forms.ModelForm):
@@ -179,8 +52,9 @@ class ClassView(IndexView):
 
     def _get_create(self, *args, **kwargs):
         self.template_name = os.path.join('teacher', 'class_create.html')
+        print self.template_name
         create_form = self.ClassForm()
-        return {'create_form': create_form,
+        return {'form': create_form,
                 'create_url': self.reverse('class_create',
                     kwargs={'request_type': 'create'})}
 
@@ -204,34 +78,59 @@ class ClassView(IndexView):
             return {'success': False,
                     'error': error,
                     'message': u'Có lỗi ở dữ liệu nhập vào'}
-
-    # Extract the request_type to call appropriate method
-    def _get(self, *args, **kwargs):
-        try:
-            req_type = kwargs['request_type']
-        except KeyError:
-            req_type = 'view'
-        if req_type in self.request_type:
-            handler = getattr(self, '_get_' + req_type,
-                    self.http_method_not_allowed)
-        else:
-            handler = self.request_type_not_allowed
-        return handler(*args, **kwargs)
     
-    def _post(self, *args, **kwargs):
+    def _get_modify(self, *args, **kwargs):
+        self.template_name = os.path.join('teacher', 'class_create.html')
+        cl_id = kwargs['class_id']
         try:
-            req_type = kwargs['request_type']
-        except KeyError:
-            req_type = 'view'
-        print 'request_type', req_type
-        if req_type in self.request_type:
-            handler = getattr(self, '_post_' + req_type,
-                    self.http_method_not_allowed)
-        else:
-            handler = self.request_type_not_allowed
-        print handler
-        return handler(*args, **kwargs)
+            cl = self.teacher.class_set.get(id=cl_id)
+        except ObjectDoesNotExist:
+            return {'success': False,
+                    'message': u'Lớp không tồn tại'}
+        modify_form = self.ClassForm(instance=cl)
+        return {'form': modify_form,
+                'modify_url': self.reverse('class_view',
+                    kwargs={'class_id': cl_id, 'request_type': 'modify'})}
 
+    def _post_modify(self, *args, **kwargs):
+        cl_id = kwargs['class_id']
+        try:
+            cl = self.teacher.class_set.get(id=cl_id)
+        except ObjectDoesNotExist:
+            return {'success': False,
+                    'message': u'Lớp không tồn tại'}
+        modify_form = self.ClassForm(self.request.POST.copy(),
+                instance=cl)
+        if modify_form.is_valid():
+            cl = modify_form.save(self.teacher)
+            return {'message': u'Bạn vừa cập nhật thành công lớp học',
+                    'success': True,
+                    'class_name': cl.name,
+                    'class_url': self.reverse('class_view',
+                        kwargs={'class_id': cl.id, 'request_type': 'view'}),
+                    'class_modify': self.reverse('class_view',
+                        kwargs={'class_id': cl.id, 'request_type': 'modify'}),
+                    'class_remove': self.reverse('class_view',
+                        kwargs={'class_id': cl.id, 'request_type': 'remove'})}
+        else:
+            error = {}
+            for k, v in modify_form.errors.items():
+                error[modify_form[k].auto_id] = modify_form.error_class.as_text(v)
+            return {'success': False,
+                    'error': error,
+                    'message': u'Có lỗi ở dữ liệu nhập vào'}
+
+    def _post_remove(self, *args, **kwargs):
+        cl_id = kwargs['class_id']
+        try:
+            cl = self.teacher.class_set.get(id=cl_id)
+        except ObjectDoesNotExist:
+            return {'success': False,
+                    'message': u'Lớp không tồn tại'}
+        cl.delete()
+        return {'success': True,
+                'message': u'Bạn đã xóa lớp %s' % cl.name}
+    
 class RegisterView(TemplateView):
     template_name = os.path.join('teacher', 'register.html')
 
