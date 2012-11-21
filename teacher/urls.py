e340bccf2211b11b1c7ac3a1675695fbc269e882
loilt_54 <loi.luuthe@gmail.com>
2012-11-22 00:07:16 +0700

    restore some stuffs

--- teacher/urls.py
+++ teacher/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from teacher.views import IndexView, ClassView, StudentView
+from teacher.views import IndexView, ClassView, StudentView, MarkView
 
 urlpatterns = patterns('',
         url(r'^$', IndexView.as_view(), name='teacher_index'),
@@ -11,4 +11,8 @@ urlpatterns = patterns('',
             StudentView.as_view(), name='student_create'),
         url(r'class/(?P<class_id>\d+?)/student/(?P<student_id>\d+)/(?P<request_type>modify|remove|view)$',
             StudentView.as_view(), name='student_view'),
+        url(r'class/(?P<class_id>\d+?)/student/(?P<student_id>\d+)/mark/(?P<request_type>create)$',
+            MarkView.as_view(), name='mark_create'),
+        url(r'class/(?P<class_id>\d+?)/student/(?P<student_id>\d+)/mark/(?P<mark_id>\d+)/(?P<request_type>modify|remove)$',
+            MarkView.as_view(), name='mark_view'),
 )
